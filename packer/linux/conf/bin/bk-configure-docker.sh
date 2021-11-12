5617c4f274e52eec73e49f9e17431a93ab4d27ef
Nathan Woodhull <woodhull@gmail.com>
2021-11-12 16:39:02 -0500

    This was written weeks ago, but not pushed, and is what we have been running.

--- packer/linux/conf/bin/bk-configure-docker.sh
+++ packer/linux/conf/bin/bk-configure-docker.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 # Write to system console and to our log file
 # See https://alestic.com/2010/12/ec2-user-data-output/
 exec > >(tee -a /var/log/elastic-stack.log|logger -t user-data -s 2>/dev/console) 2>&1
+echo "Configuring docker..."
 
 # Set user namespace remapping in config
 if [[ "${DOCKER_USERNS_REMAP:-false}" == "true" ]] ; then
@@ -23,22 +24,3 @@ if [[ "${BUILDKITE_ENABLE_INSTANCE_STORAGE:-false}" == "true" ]] ; then
   cat <<< "$(jq '."data-root"="/mnt/ephemeral/docker"' /etc/docker/daemon.json)" > /etc/docker/daemon.json
 fi
 
-# restart the docker service to ensure config file is read in.
-sudo systemctl restart docker
-
-# wait for docker to start
-next_wait_time=0
-until docker ps || [ $next_wait_time -eq 5 ]; do
-	sleep $(( next_wait_time++ ))
-done
-
-if ! docker ps ; then
-  echo "Failed to contact docker"
-  exit 1
-fi
-
-# allow failures while warming the images we use.
-docker pull circleci/postgres:12-postgis-ram || true
-docker pull ruby:2.7.4-alpine || true
-docker pull docker.elastic.co/elasticsearch/elasticsearch:7.9.3 || true
-docker pull circleci/redis:6-alpine || true
